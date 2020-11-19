4846df3cfc31551a25735d86baee52d29b83fa0c
Bogdan Falk <bogdan37falk@gmail.com>
2020-11-19 11:10:56 +0200

    Add project files.

---
+++ EmergencyMeeting/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace EmergencyMeeting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = global::EmergencyMeeting.Properties.Resources.EmergencyMeeting;
+            this.pictureBox1.InitialImage = global::EmergencyMeeting.Properties.Resources.EmergencyMeeting;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1280, 720);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ EmergencyMeeting/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Runtime.InteropServices;
+
+namespace EmergencyMeeting
+{
+    public partial class Form1 : Form
+    {
+
+
+        MqttClient client;
+        string clientId;
+
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private const UInt32 SWP_NOSIZE = 0x0001;
+        private const UInt32 SWP_NOMOVE = 0x0002;
+        private const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+            BackColor = Color.Red;
+            TransparencyKey = Color.Red;
+            NotifyIcon trayIcon = new NotifyIcon();
+            trayIcon.Icon = new Icon(@"emergencyIco.ico");
+            trayIcon.Visible = true;
+            ShowInTaskbar = false;
+            ContextMenu contextMenu1 = new System.Windows.Forms.ContextMenu();
+            MenuItem menuItem1 = new System.Windows.Forms.MenuItem();
+
+            // Initialize contextMenu1
+            contextMenu1.MenuItems.AddRange(
+                        new System.Windows.Forms.MenuItem[] { menuItem1 });
+
+            // Initialize menuItem1
+            menuItem1.Index = 0;
+            menuItem1.Text = "E&xit";
+            menuItem1.Click += new System.EventHandler(menuItem1_Click);
+
+            trayIcon.ContextMenu = contextMenu1;
+
+            Rectangle screen = Screen.PrimaryScreen.WorkingArea;
+            int w = Width >= screen.Width ? screen.Width : (screen.Width + Width) / 2;
+            int h = Height >= screen.Height ? screen.Height : (screen.Height + Height) / 2;
+            this.Location = new Point((screen.Width - w) / 2, (screen.Height - h) / 2);
+            this.Size = new Size(w, h);
+            pictureBox1.Location = new Point((screen.Width - w) / 2, (screen.Height - h) / 2);
+            pictureBox1.Size = new Size(w, h);
+
+
+            string BrokerAddress = "broker.mqttdashboard.com";
+            client = new MqttClient(BrokerAddress);
+      
+            // register a callback-function (we have to implement, see below) which is called by the library when a message was received
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            // use a unique id as client id, each time we start the application
+            clientId = Guid.NewGuid().ToString();
+
+            client.Connect(clientId);
+
+           
+            // whole topic
+            string Topic = "/emergencyVizuina/test";
+
+            // subscribe to the topic with QoS 2
+            client.Subscribe(new string[] { Topic }, new byte[] { 2 });   // we need arrays as parameters because we can subscribe to different topics with one call
+           
+            
+           
+        }
+
+        protected void menuItem1_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            this.Hide();
+            base.OnShown(e);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            client.Disconnect();
+
+            base.OnClosed(e);
+         
+        }
+
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
+
+            Invoke((MethodInvoker)(() => Visible = true));
+            
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();        
+            player.SoundLocation = @"sound.wav";
+            player.Play();
+            System.Threading.Thread.Sleep(4000);
+            Invoke((MethodInvoker)(() => Visible = false));
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
